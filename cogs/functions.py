74f00581e0a1468fbd49053e55a18829bdbf3b25
KlassiC-Man <parth.11.gulati@gmail.com>
2021-04-18 15:18:41 +0530

    SOme changes

--- cogs/functions.py
+++ cogs/functions.py
@@ -8,8 +8,9 @@ class Functions(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.content.startswith == self.bot.user.mention:
+        if message.content.startswith == self.bot.mention:
             await message.channel.send(f"Hello My name is {self.bot.user}, and my prefix is `%`. Thank you!")
+
 
 def setup(bot):
     bot.add_cog(Functions(bot))
